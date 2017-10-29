d0d3f0b8a72da474d3b988532e82240af37b2fdd
Luke Marsden <luke@weave.works>
2017-10-29 14:54:27 +0100

    Delete the right thing

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ version=$1
 curl -sSL -o Docker.dmg https://download.docker.com/mac/$version/Docker.dmg
 hdiutil mount Docker.dmg
 
-sudo rm -rf /Applications/Docker
+sudo rm -rf /Applications/Docker.app
 sudo cp -R /Volumes/Docker/Docker.app /Applications
 
 hdiutil unmount /Volumes/Docker
